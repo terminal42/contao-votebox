41082b3402ca702ec1ae7aa5fad2ec41d5f3280c
Yanick Witschi <yanick.witschi@terminal42.ch>
2013-09-30 14:53:44 +0200

    Adjusted language files

--- languages/de/default.php
+++ languages/de/default.php
@@ -32,23 +32,22 @@
 /**
  * Miscellaneous
  */
-$GLOBALS['TL_LANG']['MSC']['vb_no_ideas']	= 'Für dieses Votebox-Archiv gibt es noch keine Vorschläge.';
-$GLOBALS['TL_LANG']['MSC']['vb_no_idea']	= 'Diese ID existiert für dieses Votebox-Archiv leider nicht.';
+$GLOBALS['TL_LANG']['MSC']['vb_no_ideas']    = 'Für dieses Votebox-Archiv gibt es noch keine Vorschläge.';
+$GLOBALS['TL_LANG']['MSC']['vb_no_idea']     = 'Diese ID existiert für dieses Votebox-Archiv leider nicht.';
 
 /**
  * Votebox new idea form labels
  */
-$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['title']	= 'Titel';
-$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['text']		= 'Idee';
-$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['captcha']	= 'Sicherheitsfrage';
-$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['submit']	= 'Senden';
+$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['title']        = 'Titel';
+$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['text']         = 'Idee';
+$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['captcha']      = 'Sicherheitsfrage';
+$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['submit']       = 'Senden';
 
 /**
  * Votebox reader
  */
-$GLOBALS['TL_LANG']['MSC']['vb_vote']					= 'Für diese Idee stimmen';
-$GLOBALS['TL_LANG']['MSC']['vb_already_voted']			= 'Für diese Idee haben Sie bereits abgestimmt!';
-$GLOBALS['TL_LANG']['MSC']['vb_successfully_voted']		= 'Ihre Stimme wurde gespeichert!';
-$GLOBALS['TL_LANG']['MSC']['vb_unvote']					= 'Dieser Idee die Stimme entziehen';
-$GLOBALS['TL_LANG']['MSC']['vb_successfully_unvoted']	= 'Ihre Stimme wurde erfolgreich entzogen!';
-$GLOBALS['TL_LANG']['MSC']['vb_too_many_votes']         = 'Sie haben das Maximum an Stimmen für dieses Archiv bereits aufgebraucht.';
+$GLOBALS['TL_LANG']['MSC']['vb_vote']                       = 'Für diese Idee stimmen';
+$GLOBALS['TL_LANG']['MSC']['vb_already_voted']              = 'Für diese Idee haben Sie bereits abgestimmt oder Sie haben das Maximum an Stimmen für dieses Archiv bereits aufgebraucht!';
+$GLOBALS['TL_LANG']['MSC']['vb_successfully_voted']         = 'Ihre Stimme wurde gespeichert!';
+$GLOBALS['TL_LANG']['MSC']['vb_unvote']                     = 'Dieser Idee die Stimme entziehen';
+$GLOBALS['TL_LANG']['MSC']['vb_successfully_unvoted']       = 'Ihre Stimme wurde erfolgreich entzogen!';
--- languages/en/default.php
+++ languages/en/default.php
@@ -32,23 +32,22 @@
 /**
  * Miscellaneous
  */
-$GLOBALS['TL_LANG']['MSC']['vb_no_ideas']	= 'There are no ideas for this votebox archive yet.';
-$GLOBALS['TL_LANG']['MSC']['vb_no_idea']	= 'This ID does not exist in this votebox archive.';
+$GLOBALS['TL_LANG']['MSC']['vb_no_ideas']    = 'There are no ideas for this votebox archive yet.';
+$GLOBALS['TL_LANG']['MSC']['vb_no_idea']     = 'This ID does not exist in this votebox archive.';
 
 /**
  * Votebox new idea form labels
  */
-$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['title']	= 'Title';
-$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['text']		= 'Idea';
-$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['captcha']	= 'Security question';
-$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['submit']	= 'Send';
+$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['title']        = 'Title';
+$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['text']         = 'Idea';
+$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['captcha']      = 'Security question';
+$GLOBALS['TL_LANG']['MSC']['form_votebox_new_idea']['submit']       = 'Send';
 
 /**
  * Votebox reader
  */
-$GLOBALS['TL_LANG']['MSC']['vb_vote']					= 'Vote for this idea';
-$GLOBALS['TL_LANG']['MSC']['vb_already_voted']			= 'You have already voted for this idea!';
-$GLOBALS['TL_LANG']['MSC']['vb_successfully_voted']		= 'Your vote has been saved!';
-$GLOBALS['TL_LANG']['MSC']['vb_unvote']					= 'Withdraw vote from this idea';
-$GLOBALS['TL_LANG']['MSC']['vb_successfully_unvoted']	= 'Your vote has been withdrawn from this idea!';
-$GLOBALS['TL_LANG']['MSC']['vb_too_many_votes']         = 'You have used up all of your votes for this archive.';
+$GLOBALS['TL_LANG']['MSC']['vb_vote']                       = 'Vote for this idea';
+$GLOBALS['TL_LANG']['MSC']['vb_already_voted']              = 'You have already voted for this idea or you have used up all of your votes for this archive!';
+$GLOBALS['TL_LANG']['MSC']['vb_successfully_voted']         = 'Your vote has been saved!';
+$GLOBALS['TL_LANG']['MSC']['vb_unvote']                     = 'Withdraw vote from this idea';
+$GLOBALS['TL_LANG']['MSC']['vb_successfully_unvoted']       = 'Your vote has been withdrawn from this idea!';
